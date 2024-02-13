366aba73aca04ae594f52e3a6f09fc4f2fd20bf0
Ben <benc030602@gmail.com>
2024-02-13 11:46:47 +0000

    Added in kaggle book dataset, extra random strings for testing

--- Normalized Compression Distance.py
+++ Normalized Compression Distance.py
@@ -1,8 +1,15 @@
 import zlib
 
 #Data to be classified
-seq1 = "ABCDEFGHIJK".encode('latin-1')
-seq2 = "ABCDEFGHIJK".encode('latin-1')
+#seq1 = "g7e87urhghh488guww974jb9bih86h3j8duishisuhvihvspiuhsvasdvsd".encode('latin-1')
+#seq2 = "g7e87urhghh488guww974jb9bih86h3j8duishisuhvihvspiuhsvasdvsd".encode('latin-1')
+
+#seq1 = "1010101010101010101010101010101010101".encode('latin-1')
+#seq2 = "0101010101010101010101010101010101010".encode('latin-1')
+
+seq1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum".encode('latin-1')
+seq2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum".encode('latin-1')
+
 
 #Compresses strings, retrieves lengths
 seq1_compressed = len(zlib.compress(seq1))
