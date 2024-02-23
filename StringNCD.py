2178d515a431429a100c823d87dd08f98d96c99b
Ben <benc030602@gmail.com>
2024-02-23 11:46:17 +0000

    stringncd train test split

--- StringNCD.py
+++ StringNCD.py
@@ -22,3 +22,23 @@ booksDS = pd.read_csv('books.csv')
 #Prints a specific row from dataset
 #print(booksDS.iloc[1])
 
+#x = data, y = labels
+X= booksDS['summary'] 
+y= booksDS['genre'] 
+
+#Train-test split. Random_state used for reproducability or results. Test_size of 0.2 means test size is 0.2 of population.
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1, shuffle=True)
+
+
+# printing out train and test sets 
+print('X_train : ') 
+print(X_train.head()) 
+print('') 
+print('X_test : ') 
+print(X_test.head()) 
+print('') 
+print('y_train : ') 
+print(y_train.head()) 
+print('') 
+print('y_test : ') 
+print(y_test.head())
